17a59d51038415f666e769e1aa666548d177c609
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2021-03-06 09:45:52 +0000

    Update server.py: Ignore all exceptions

--- server.py
+++ server.py
@@ -11,25 +11,28 @@ class GameServerHandler(socketserver.BaseRequestHandler):
         if isPlayFirst:
             gameInstance.setNextTurn(callBot(gameInstance.getInfo()))
 
-        while not gameInstance.checkGameOver():
-            self.request.sendall(bytes(gameInstance.getInfo(), "ASCII"))
+        try:
+            while not gameInstance.checkGameOver():
+                self.request.sendall(bytes(gameInstance.getInfo(), "ASCII"))
 
-            try:
-                ret = str(self.request.recv(8), "ASCII")
-            except:
-                self.request.sendall(bytes("ERROR: INVALID ASCII STRING ~ " + repr(ret), "ASCII"))
-                return
-            if ret != "NULL" and re.fullmatch("\w\d", ret) is None:
-                self.request.sendall(bytes("ERROR: INVALID INPUT ~ " + repr(ret), "ASCII"))
-                return
-            if not gameInstance.setNextTurn(ret):
-                self.request.sendall(bytes("ERROR: INVALID MOVE ~ " + repr(ret), "ASCII"))
-                return
+                try:
+                    ret = str(self.request.recv(8), "ASCII")
+                except:
+                    self.request.sendall(bytes("ERROR: INVALID ASCII STRING ~ " + repr(ret), "ASCII"))
+                    return
+                if ret != "NULL" and re.fullmatch("\w\d", ret) is None:
+                    self.request.sendall(bytes("ERROR: INVALID INPUT ~ " + repr(ret), "ASCII"))
+                    return
+                if not gameInstance.setNextTurn(ret):
+                    self.request.sendall(bytes("ERROR: INVALID MOVE ~ " + repr(ret), "ASCII"))
+                    return
 
-            if not gameInstance.checkGameOver():
-                gameInstance.setNextTurn(callBot(gameInstance.getInfo()))
+                if not gameInstance.checkGameOver():
+                    gameInstance.setNextTurn(callBot(gameInstance.getInfo()))
 
-        self.request.sendall(bytes(gameInstance.getFinalResult(), "ASCII"))
+            self.request.sendall(bytes(gameInstance.getFinalResult(), "ASCII"))
+        except:
+            return
 
 if __name__ == "__main__":
     HOST, PORT = "", 14003
