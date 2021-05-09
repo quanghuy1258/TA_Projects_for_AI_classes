a7ea989d60717a0633549c57d0aee13ef52ee6a6
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2021-05-09 10:00:23 +0000

    Server: Add timeout 60s

--- server.py
+++ server.py
@@ -4,6 +4,11 @@ from init import Game
 from bot import callBot
 
 class GameServerHandler(socketserver.BaseRequestHandler):
+    def setup(self):
+        self.old_timeout = self.request.gettimeout()
+        self.new_timeout = 60
+        self.request.settimeout(self.new_timeout)
+
     def handle(self):
         gameInstance = Game()
 
@@ -33,6 +38,9 @@ class GameServerHandler(socketserver.BaseRequestHandler):
             self.request.sendall(bytes(gameInstance.getFinalResult(), "ASCII"))
         except:
             return
+
+    def finish(self):
+        self.request.settimeout(self.old_timeout)
 
 if __name__ == "__main__":
     HOST, PORT = "", 14003
